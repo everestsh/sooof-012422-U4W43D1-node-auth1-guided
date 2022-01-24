6863edfb0f47169ba520985d8cd2977a9a7cac52
sooof <sooof@me.com>
2022-01-24 15:29:46 -0800

    init

---
+++ api/server.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+
+const usersRouter = require('./users/users-router.js')
+
+const server = express()
+
+server.use(express.static(path.join(__dirname, '../client')))
+server.use(express.json())
+
+server.use('/api/users', usersRouter)
+
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'))
+})
+
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found!' })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
+module.exports = server
